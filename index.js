5749c23bc496f5c28071eb27819666e0a5c38e43
AxelMlz <axel.fm.melezan@gmail.com>
2022-03-14 12:03:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express =require('express');
+const app = express();
+const port = 8000;
+app.use(express.json());
+
+
+
+app.listen(posr, () => {
+    console.log (`Server started on port:` + port)
+});
